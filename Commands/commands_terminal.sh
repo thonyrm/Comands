4d0b77c07c65a299ae064c19561a194a7a46b8d5
Thony Dev <ramosm0106@gmail.com>
2025-11-11 10:13:05 -0300

    Terminal commands

---
+++ Commands/commands_terminal.sh
@@ -0,0 +1,6 @@
+# COMANDO DE TERMINAL 
+# Permisos de archivos
+
+chmod       # Cambia permisos de un archivo
+chown       # Cambia propietario de un archivo
+sudo        # Ejecuta comandos como administrador 
